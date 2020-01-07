3ec50f05be32c2af98561daba007b8d7b4f9c55c
hakankuzi <55097049+hakankuzi@users.noreply.github.com>
2020-01-07 11:01:20 +0000

    hot fixes

--- app/public/controllers/IndexCtrl.js
+++ app/public/controllers/IndexCtrl.js
@@ -3,7 +3,7 @@ var IndexCtrl = angular.module('IndexCtrl', []);
 IndexCtrl.controller('IndexController', function ($rootScope, $location, AuthUser) {
     var vm = this;
     vm.header = {};
-    vm.header.brand = "TimeToHelp";
+    vm.header.brand = "Tokbox Sample";
     vm.header.title = "Management";
 
 });
